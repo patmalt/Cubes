c30fd1ce84408465b5e5e54f3cd1039bd5adc0a4
Patrick Maltagliati <patmalt@gmail.com>
2020-12-01 15:51:39 -0600

    Initial Commit

---
+++ Cubes/CubesApp.swift
@@ -0,0 +1,17 @@
+//
+//  CubesApp.swift
+//  Cubes
+//
+//  Created by Patrick Maltagliati on 12/1/20.
+//
+
+import SwiftUI
+
+@main
+struct CubesApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
